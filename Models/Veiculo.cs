81cff362fddd4424d4b10702630c92d768c70acc
Binho Rocha <binhuhue@gmail.com>
2026-04-26 22:07:20 -0300

    feat: implementacao do model Veiculo com data annotations

---
+++ Models/Veiculo.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC.Dev.2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a placa!")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o ano de fabricação!")]
+        public int AnoFabricacao { get; set; }
+    }
+}
